f7e146c10a5642be455668580bdbf7e6bc6a5cdc
Joana Pinheiro <joana_mafalda_magalhaes@hotmail.com>
2022-03-18 16:58:04 +0000

    18/03

--- index.php
+++ index.php
@@ -45,15 +45,11 @@
 					<nav id="menu">
 						<h2>Menu</h2>
 						<ul>
-							<li><a href="index.html" class="active">Home</a></li>
+						<li><a href="index.php" class="active">Home</a></li>
 
-							<li><a href="blog.html">Blog</a></li>
+						<li><a href="terms.php">Terms</a></li>
 
-							<li><a href="about.html">About</a></li>
-
-							<li><a href="team.html">Authors</a></li>
-
-							<li><a href="contact.html">Contact Us</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
 						</ul>
 					</nav>
 
@@ -92,17 +88,12 @@
 
 						<div class="inner">
 							<!-- About Us -->
-							<header id="inner">
-								<h1 style="margin: 0">Projeto ATW</h1>
-								<small style="margin: 0">Criação de um site para a cadeira de ATW.</small>
-							</header>
-							<br>
-							<br>
 							<h2 class="h2">Latest terms posted&nbsp;<i class="fa fa-level-down"></i></h2>
 							<br>
 							<div class="row">
 								<div class="col-sm-4 text-center">
-									<h2 class="m-n"><a href="#">Title 1.</a></h2>
+
+								<h2 class="m-n"><a href="#">Title 1.</a></h2>
 									<br>
 									<p> John Doe &nbsp;|&nbsp; 12/06/2020 10:30</p>
 								</div>
@@ -138,50 +129,21 @@
 								</div>
 							</div>
 
-							<p class="text-center"><a href="terms.html">Read More &nbsp;<i class="fa fa-long-arrow-right"></i></a></p>
+							<p class="text-center"><a href="terms.php">Read More &nbsp;<i class="fa fa-long-arrow-right"></i></a></p>
 						</div>
 					</div>
 
 				<!-- Footer -->
 					<footer id="footer">
 						<div class="inner">
-							<section>
-								<h2>Contact Us</h2>
-								<form method="post" action="#">
-									<div class="fields">
-										<div class="field half">
-											<input type="text" name="name" id="name" placeholder="Name" />
-										</div>
-
-										<div class="field half">
-											<input type="text" name="email" id="email" placeholder="Email" />
-										</div>
-
-										<div class="field">
-											<input type="text" name="subject" id="subject" placeholder="Subject" />
-										</div>
-
-										<div class="field">
-											<textarea name="message" id="message" rows="3" placeholder="Message"></textarea>
-										</div>
-
-										<div class="field text-right">
-											<label>&nbsp;</label>
-
-											<ul class="actions">
-												<li><input type="submit" value="Send" class="primary" /></li>
-											</ul>
-										</div>
-									</div>
-								</form>
-							</section>
+							
 							<section>
 								<h2>Contact Info</h2>
 
 								<ul class="alt">
-									<li><span class="fa fa-envelope-o"></span> <a href="#">contact@sargotech.com</a></li>
-									<li><span class="fa fa-phone"></span> +351 912 345 678 </li>
-									<li><span class="fa fa-map-pin"></span> </li>
+									<li><span class="fa fa-envelope-o"></span> <a href="#">contact@company.com</a></li>
+									<li><span class="fa fa-phone"></span> +1 333 4040 5566 </li>
+									<li><span class="fa fa-map-pin"></span> 212 Barrington Court New York, ABC 10001 United States of America</li>
 								</ul>
 
 								<h2>Follow Us</h2>
@@ -195,7 +157,8 @@
 							</section>
 
 							<ul class="copyright">
-								<li>Copyright © 2022 SARGOTECH </li>
+							<li>Copyright © 2020 Company Name </li>
+							<li>Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></li>
 							</ul>
 						</div>
 					</footer>
